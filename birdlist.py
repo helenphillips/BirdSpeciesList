8e7c3315a847f31a7c2e2559a38a104dd804df8b
Helen Phillips <helen.r.phillips@googlemail.com>
2014-01-10 10:08:57 +0000

    adding commas to separate text file

--- birdlist.py
+++ birdlist.py
@@ -74,7 +74,7 @@ for statusObj in results:
         date_obj = datetime.strptime(date, '%a %b %d %H:%M:%S +0000 %Y')
         d = date_obj.strftime('%d/%m/%Y %I:%M%p')
         txt = statusObj.text[16:]
-        fp.write('\n' + '%s %s' % (date, txt))
+        fp.write('\n' + '%s, %s' % (date, txt))
     else:
         date = statusObj.created_at
         date_obj = datetime.strptime(date, '%a %b %d %H:%M:%S +0000 %Y')
@@ -89,7 +89,7 @@ for statusObj in results:
         query = "INSERT INTO %s (Date, Species, Latitude, Longitude) VALUES ('%s', '%s', '%s', '%s')" % (table_id, d, txt, lat, lng)
         print(service.query().sql(sql=query).execute())
 
-        fp.write('\n' + '%s %s %s %s' % (date, txt, lat, lng))
+        fp.write('\n' + '%s, %s, %s, %s' % (date, txt, lat, lng))
 fp.close()
 
 
